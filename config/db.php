aa51e19818398863c561b7feb1623bf20a32feb7
ArmanMar97 <65087621+ArmanMar97@users.noreply.github.com>
2020-10-12 17:29:00 +0400

    db methods added

--- config/db.php
+++ config/db.php
@@ -1,21 +1,107 @@
 <?php
 
 
-class DB{
+class DB
+{
 
-    const HOST = "localhost";
+    const  HOST = "localhost";
     const USER = "root";
     const PASSWORD = "";
     const DB = "feedback";
 
-    public static function connect(){
+    public static function connect( $dbName = self::DB){
         $host = self::HOST;
         $user = self::USER;
         $password = self::PASSWORD;
         $db = self::DB;
 
-        $conn = mysqli_connect(self::HOST,self::USER,self::PASSWORD,self::DB);
+        if (isset($dbName)){
+            $db = $dbName;
+        }
+
+
+        $conn = mysqli_connect($host, $user, $password, $db);
         return $conn;
+
+    }
+
+    public static function createDatabase( $dbName){
+        $conn = self::connect();
+        $sql = "CREATE DATABASE $dbName";
+
+        if ($conn){
+            mysqli_query($conn,$sql);
+        }
+        else{
+            echo "Connection error!";
+        }
+
+    }
+
+    public static function createPostsTable( $tableName,$dbName=self::DB){
+        if (isset($dbName)){
+            $dbName = $dbName;
+        }
+
+        $conn = self::connect($dbName);
+
+        $sql = "CREATE TABLE IF NOT EXISTS `$tableName` (
+                  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                    `first_name` varchar(255) DEFAULT NULL,
+                    `last_name` varchar(255) DEFAULT NULL,
+                      `email` varchar(255) DEFAULT NULL,
+                        `message` text DEFAULT NULL,
+                        `sent_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+                        UNIQUE KEY `id` (`id`)
+                        ) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8mb4";
+        if ($conn){
+            $result = mysqli_query($conn,$sql);
+        }
+        else{
+            echo "Connection error!";
+        }
+    }
+
+    public static function createAdminTable($dbName=self::DB){
+
+        if (isset($dbName)){
+            $dbName = $dbName;
+        }
+        $conn = self::connect($dbName);
+
+        $sql = "CREATE TABLE IF NOT EXISTS `admin` (
+                  `username` varchar(255) DEFAULT NULL,
+                  `password` varchar(255) DEFAULT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+
+        if ($conn){
+            $result = mysqli_query($conn,$sql);
+        }
+        else{
+            echo "Connection error!";
+        }
+
+    }
+
+    public static function createAdminUser( $username, $password, $dbName=self::DB){
+        if (isset($dbName)){
+            $dbName=$dbName;
+        }
+        $conn = self::connect($dbName);
+
+        $sql = "INSERT INTO admin (username, password)
+                SELECT * FROM (SELECT '$username', '$password') AS tmp
+                WHERE NOT EXISTS (
+                    SELECT username FROM admin WHERE username = '$username'
+                ) LIMIT 1;";
+
+        if ($conn){
+            $result = mysqli_query($conn,$sql);
+        }
+        else{
+            echo "Connection error!";
+        }
+
     }
 
 }
